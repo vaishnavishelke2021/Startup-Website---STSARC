2e1aa184bc6ec6b38a3808250e3bb88cd654dbf2
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-10-31 09:06:35 +0530

    Updated: Marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Marquee from "react-fast-marquee";
 import { FaBrain, FaGlobe, FaLink } from "react-icons/fa";
+import { SiUblockorigin } from "react-icons/si";
 
 const Card = ({ icon, heading, subtext }) => {
   return (
     <div className="flex flex-col items-center justify-center mr-6 w-[350px] h-48 p-4 bg-gray-100/60 rounded-xl border-gray-200 transition-transform transform hover:bg-light">
-      <div className="text-2xl mb-2 text-orange-500">{icon}</div>
+      <div className="text-[22px] mb-3 text-orange-500">{icon}</div>
       <h3 className="text-lg font-bold text-gray-800 raleway">{heading}</h3>
       <p className="text-sm text-center text-zinc-600 mt-2">{subtext}</p>
     </div>
@@ -27,7 +28,7 @@ const MarqueeCards = () => {
         "Next-gen internet that puts users in control and ensures privacy and security.",
     },
     {
-      icon: <FaLink />,
+      icon: <SiUblockorigin />,
       heading: "Blockchain",
       subtext:
         "Secure digital ledger that records transactions transparently and without intermediaries.",
@@ -41,7 +42,7 @@ const MarqueeCards = () => {
       {/* Right Gradient */}
       <div className="absolute right-0 top-0 h-full w-16 bg-gradient-to-l from-white to-transparent z-10 pointer-events-none"></div>
 
-      <Marquee speed={70} gradient={false} pauseOnHover={true}>
+      <Marquee speed={80} gradient={false} pauseOnHover={true}>
         {cardData.map((card, index) => (
           <Card
             key={index}
